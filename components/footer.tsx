eb71b5b0679d81a17fe069dd3c5f301c4dc8c011
ta-02 <talal.gl2004@gmail.com>
2024-07-02 20:05:36 -0400

    changed some stuff

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,4 +11,3 @@ export const Footer = () => {
     </footer>
   );
 };
-
--- src/app/createNote/page.tsx
+++ src/app/createNote/page.tsx
@@ -1,5 +1,10 @@
 const createNote = () => {
-  return <div>Create Note</div>;
+  return (
+    <main className="space-y-8">
+      <h1 className="container space-y-8">Add a note</h1>
+      <form></form>
+    </main>
+  );
 };
 
 export default createNote;
