754da8ed032eff8f60fa51d9df06098cbd41ad7f
chanr335 <76451685+chanr335@users.noreply.github.com>
2024-07-07 13:12:57 -0400

    Update

--- src/app/createNote/page.tsx
+++ src/app/createNote/page.tsx
@@ -2,10 +2,12 @@ const createNote = () => {
   return (
     <main className="space-y-8">
       <h1 className="flex border-b p-5 justify-center font-semibold">Create A New Note.</h1>
-          <form className="w-full flex border-b p-10 justify-center flex-col space-y-4">
+          <form className="w-full flex border-b p-10 justify-center flex-col space-y-4"
+                action="">
                 <input type="text" id="class" name="class" placeholder="Class"></input>
                 <input type="text" placeholder="Tags"></input>
                 <input className="p-5" type="text" id="note" name="note" placeholder="..."></input>
+                <input type="Submit" value="Submit"></input>
           </form>
     </main>
   );
